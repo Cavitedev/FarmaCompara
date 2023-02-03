da74ce725deccc8be4d57b33b75375949075741f
Carlos Víllora Tercero <carlosvillorat@gmail.com>
2023-02-03 18:38:16 +0100

    feat: :sparkles: Sacar datos de farmaciaencasa #10
    Añadir la funcionalidad para sacar datos de https://www.farmaciaencasaonline.es/corporal-cuidado-cuerpo/

--- functions/web_scrap/scrapper/scrapper.go
+++ functions/web_scrap/scrapper/scrapper.go
@@ -5,6 +5,7 @@ import (
 
 	"cloud.google.com/go/firestore"
 	"github.com/cavitedev/farma-compara/web_scrap/scrapper/dosfarma"
+	"github.com/cavitedev/farma-compara/web_scrap/scrapper/farmaciaencasa"
 	"github.com/cavitedev/farma-compara/web_scrap/scrapper/farmaciasdirect"
 	"github.com/cavitedev/farma-compara/web_scrap/scrapper/okfarma"
 )
@@ -13,12 +14,15 @@ func Scrap(website string, ref *firestore.CollectionRef) string {
 
 	log.Println("Hola scrapper")
 
-	if website == okfarma.Domain {
+	switch website {
+	case okfarma.Domain:
 		okfarma.Scrap(ref)
-	} else if website == farmaciasdirect.Domain {
+	case farmaciasdirect.Domain:
 		farmaciasdirect.Scrap(ref)
-	} else if website == dosfarma.Domain {
+	case dosfarma.Domain:
 		dosfarma.Scrap(ref)
+	case farmaciaencasa.Domain:
+		farmaciaencasa.Scrap(ref)
 	}
 
 	return "Scrapping of " + website + " complete"
